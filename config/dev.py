a977260d18d051844fcea27b21e15c585bcfc50c
Odel <martinezkener300@gmail.com>
2025-10-09 23:38:21 -0600

    CommitBack-End1.4.3 KenerOdelToruñoMartinez

--- config/dev.py
+++ config/dev.py
@@ -23,13 +23,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
--- config/prod.py
+++ config/prod.py
@@ -1,14 +1,5 @@
 from .base import *
-import os
 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
 
-# Ya debería usar SQLite de base.py
-# No necesitamos redefinir DATABASES aquí
-
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = []
